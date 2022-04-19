15168d789044eb268b9b1b35533d66baa1f2bf1f
Elia Albanese <67117955+EAlbanese@users.noreply.github.com>
2022-04-19 15:07:57 +0200

    WorkTime project add

---
+++ WorkTime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WorkTime
+{
+    //public class User
+    //{
+    //    public string Username { get; set; }
+    //    public string Email { get; set; }
+    //    public string IP { get; set; }
+    //    public int ID { get; set; }
+    //}
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            DateTime lunch = DateTime.Today.AddHours(12);
+            DateTime workEndDay = DateTime.Today.AddHours(17);
+
+            Console.WriteLine("Die Aktuelle Zeit: " + dateTime.ToString("HH:mm:ss") + " Uhr");
+            Console.WriteLine();
+            if(DateTime.Now <= lunch)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Du musst noch: " + (lunch - dateTime) + " Stunden arbeiten bis du essen kannst");
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Du musst noch: " + (workEndDay - dateTime) + " Stunden arbeiten bis du nach Hause gehen kannst");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+        }
+    }
+}
